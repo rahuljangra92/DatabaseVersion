e1a3035ef99a1199819e0c663fd06f31ad1dc04d
rahuljangra92 <rahul.jangra@sourcefuse.com>
2024-06-13 17:33:17 +0530

    Added new Sproc and new column to student table

--- Tables/dbo.Student.sql
+++ Tables/dbo.Student.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Student]
 (
 [StudentId] [int] NULL,
-[StudentCode] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
+[StudentCode] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[StudentCity] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
